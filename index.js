b96804c614572ee805245ce97e8d7e33f01dbdd1
River <+>
2022-04-26 00:15:54 -0400

    commit tree 7abcea366f2d1651bf2aebc4a6748c6126d759a8

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply =num1*num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16
+let num4 = 6
+const mod = num3%num4;
+
+const max = Math.max (1, 5, 10, 20);
